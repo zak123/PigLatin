4201daa78c47b0754f3ebfab64bac5861708be7b
zak123 <zacharym123@gmail.com>
2015-03-13 15:24:28 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Zachary Mallicoat on 3/13/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
